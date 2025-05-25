8f675d3d678d68f4fbd044581929e803d179b772
Capusta007 <dimacharnenko@gmail.com>
2025-05-26 02:35:05 +0300

    Add simple descriprion of AppConstants class

--- src/main/java/config/AppConstants.java
+++ src/main/java/config/AppConstants.java
@@ -1,5 +1,8 @@
 package config;
 
+/**
+ * Defines attribute keys used to store objects in the {@code ServletContext}.
+ */
 public class AppConstants {
 	public static final String USER_SERVICE_ATTR = "UserService";
 }
