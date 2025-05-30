3f00f8c9cb2a9ebb1e21f0bd97efc3e664077a3f
Capusta007 <dimacharnenko@gmail.com>
2025-05-30 04:51:36 +0300

    Set up AuthFilter

---
+++ src/main/java/filters/AuthFilter.java
@@ -0,0 +1,40 @@
+package filters;
+
+import java.io.IOException;
+import java.util.Set;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.SessionAttributes;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+
+	private static final Set<String> ALLOWED_PAGES = Set.of("/MiniGram/login", "/MiniGram/registration");
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+
+		String uri = req.getRequestURI();
+		System.out.println(uri);
+		if (req.getSession().getAttribute(SessionAttributes.USER_ID) == null) {
+			if (ALLOWED_PAGES.contains(uri)) {
+				chain.doFilter(request, response);
+			} else {
+				resp.sendRedirect("login");
+			}
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+}
